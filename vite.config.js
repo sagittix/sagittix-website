c1199fd579537111c19351b7a9bf8e22f1c8ce92
Sagittix <sagittix@gmail.com>
2025-10-16 14:00:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  
+  // 🔑 CRITICAL FIX: Set the base path to the repository name for GitHub Pages.
+  base: '/sagittix-website/', 
+});
